b9e8571c75d0244dab495a3f5036ed08685f67f8
DCtheTall <dylancutler@google.com>
2020-08-31 21:23:20 -0400

    matrix attributes

--- examples/cube/src/main.js
+++ examples/cube/src/main.js
@@ -1,6 +1,7 @@
 const {
   PLANE_VERTICES_TRIANGLE_STRIP,
   FloatAttribute,
+  Mat2Attribute,
   Vec2Attribute,
   Shader,
 } = require('../../../dist');
@@ -10,11 +11,23 @@ const fragmentShader = require('./fragment.glsl').default;
 
 const canvas = document.getElementById('canvas');
 
-Shader(4, vertexShader, fragmentShader, {
+const N_VERTICES = 4;
+
+Shader(N_VERTICES, vertexShader, fragmentShader, {
   attributes: {
     aPosition: Vec2Attribute('a_Position', PLANE_VERTICES_TRIANGLE_STRIP),
-    aRed: FloatAttribute('a_Red', new Float32Array([1, 1, 0, 0])),
-    aGreen: FloatAttribute('a_Green', new Float32Array([0, 1, 1, 0])),
-    aBlue: FloatAttribute('a_Blue', new Float32Array([0, 0, 1, 1])),
+    aIdentity: Mat2Attribute('a_Identity', [
+      new Float32Array([
+        1, 0, 1, 0,
+        1, 0, 1, 0,
+      ]),
+      new Float32Array([
+        0, 1, 0, 1,
+        0, 1, 0, 1,
+      ]),
+    ]),
+    aRed: FloatAttribute('a_Red', new Float32Array([1, 0, 0, 1])),
+    aGreen: FloatAttribute('a_Green', new Float32Array([0, 1, 0, 1])),
+    aBlue: FloatAttribute('a_Blue', new Float32Array([0, 0, 1, 0])),
   },
 })(canvas);
--- examples/cube/src/vertex.glsl
+++ examples/cube/src/vertex.glsl
@@ -4,10 +4,11 @@ attribute float a_Red;
 attribute float a_Green;
 attribute float a_Blue;
 attribute vec2 a_Position;
+attribute mat2 a_Identity;
 
 varying vec3 color;
 
 void main() {
-  gl_Position = vec4(a_Position, 0.0, 1.0);
+  gl_Position = vec4(a_Identity * a_Position, 0.0, 1.0);
   color = vec3(a_Red, a_Green, a_Blue);
 }
